f6e11d27d224ccb43d672e705b6d134fed4abc48
fadria <fadriacarrasco@gmail.com>
2022-02-23 12:46:48 +0100

    Impresión de variables en el HTML

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -3,11 +3,24 @@ import React from 'react';
 
 const PrimeraApp = () => {
    
+    //  Los objetos no se pueden imprimir directamente
+    const saludo = {
+        nombre: 'Federico',
+        edad: 25
+    }
+    const string = "Buenas tardes";
+    const numeros = 123.45;
+    const arreglo = [1,2,3,4,5,6]
+
     // Forma corta de devolver un Fragment, no sería necesario importar
     return (
-        <> 
-            <h1>Hola Mundo</h1>
+        <>
+            <h1>{string}</h1>
+            <h2>{numeros}</h2>
             <p>Mi primera aplicación</p>
+            <p>Los booleanos no se imprimen: {true}</p>
+            <p>{arreglo}</p>
+            <pre>{JSON.stringify(saludo, null, 3)}</pre>
         </>
     );
 
