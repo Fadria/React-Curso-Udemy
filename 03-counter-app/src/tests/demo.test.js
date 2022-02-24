8247aa3a72432dc821a6c1c9d60b72889a45f228
fadria <fadriacarrasco@gmail.com>
2022-02-24 10:54:34 +0100

    Jest - Expect - toBe

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,9 +1,16 @@
-// Nombre del test
-test( "Debe de ser true", () => {
-    const isActive = false;
+// Agrupación de todas las pruebas de este fichero
+describe("Pruebas en el archivo demo.test.js", () =>{
 
-    // Si es falso lanzaremos un error que aparecerá por consola
-    if(!isActive) {
-        throw new Error("No está activo");
-    }
-})
+    // Nombre del test
+    test( "Deben de ser iguales los string", () => {
+        // 1. Inicialización
+        const mensaje = "Hola Mundo";
+
+        // 2. Estímulo
+        const mensaje2 = `Hola Mundo`;
+
+        // 3. Observar el comportamiento
+        expect(mensaje).toBe(mensaje2); // Internamente hace la comparación de equidad ===
+    })
+
+});
