632007d0125756048bd133712c0d6bbc92d7ca69
fadria <fadriacarrasco@gmail.com>
2022-03-01 12:59:04 +0100

    Simular eventos - Click

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -4,8 +4,14 @@ import React from 'react';
 
 describe("Pruebas en el <CounterApp />", () => {
 
+    let wrapper = shallow(<CounterApp />);
+
+    // Función que se ejecuta con cada test
+    beforeEach( () => {
+        wrapper = shallow(<CounterApp />);
+    })
+
     test("Debe de mostrar <CounterApp /> correctamente", () =>{
-        const wrapper = shallow(<CounterApp />);
         expect(wrapper).toMatchSnapshot();
     })
 
@@ -15,5 +21,22 @@ describe("Pruebas en el <CounterApp />", () => {
         const counterText = wrapper.find('h2').text().trim();
 
         expect(counterText).toBe('100');
+    })
+
+    test("Debe de incrementar con el botón +1", () =>{
+        const btn1 = wrapper.find('button').at(0).simulate('click');
+
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('1');
+    })
+
+    // Las pruebas se ejecutan una detrás de otra, en este caso pasaríamos del valor 1 al 0 ya que en la anterior sumamos
+    test("Debe de incrementar con el botón -1", () =>{
+        const btn1 = wrapper.find('button').at(1).simulate('click');
+
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('0');
     })
 })
