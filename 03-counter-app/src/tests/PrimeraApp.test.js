97b7f3bb00ed05e8627507acf9f7a4d007acc9cf
fadria <fadriacarrasco@gmail.com>
2022-03-01 12:19:46 +0100

    Enzyme - Testing unit

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+//import { render } from "@testing-library/react";
 import PrimeraApp from "../PrimeraApp";
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 
 describe("Pruebas en <PrimeraApp />", () => {
-    test('Debe de mostrar el mensaje "Hola desde el index.js"', () => {
+    /*test('Debe de mostrar el mensaje "Hola desde el index.js"', () => {
         const saludo = "Hola desde el index.js";
 
         const {getByText} = render(<PrimeraApp saludo={saludo} />);
 
         expect(getByText(saludo)).toBeInTheDocument();
+    })*/
+
+    test("Debe de mostrar <PrimeraApp /> correctamente", () => {
+        const saludo = "Hola desde el index.js";
+        const wrapper = shallow(<PrimeraApp saludo={saludo} />);
+
+        expect(wrapper).toMatchSnapshot();
     })
 })
