eb8d66edaeba905efee07ba265edbf05083fea1b
fadria <fadriacarrasco@gmail.com>
2022-03-02 12:55:17 +0100

    useFetchGifs - obtener imágenes y bandera de carga

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,34 +1,25 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
-///import { getGifs } from '../helpers/getGifs';
-//import { GifGridItem } from './GifGridItem';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({categoria}) => {
     
-    const {loading} = useFetchGifs();
-
-
-
-    /*const [images, setImages] = useState([])
-    
-    useEffect(() => {
-        getGifs(categoria)
-        .then(imgs => {setImages(imgs)});
-    }, [categoria])*/
+    const {data:images, loading} = useFetchGifs(categoria);
 
     return (
         <>
             <h3>{categoria}</h3>
 
-            {loading ? 'Cargando...' : 'Data cargada'}
-            {/*<div className='card-grid'>
-                {images.map(img => (
-                    <GifGridItem 
-                        key={img.id}
-                        {...img}
-                    />
-                ))}
-                </div>*/}
+                {loading && <p>Cargando...</p>}
+
+                <div className='card-grid'>
+                    {images.map(img => (
+                        <GifGridItem 
+                            key={img.id}
+                            {...img}
+                        />
+                    ))}
+                </div>
         </>
     )
 }
--- 04-gif-expert-app/src/hooks/useFetchGifs.js
+++ 04-gif-expert-app/src/hooks/useFetchGifs.js
@@ -1,18 +1,27 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import {getGifs} from '../helpers/getGifs'
 
-export const useFetchGifs = () => {
+export const useFetchGifs = (categoria) => {
 
     const [state, setState] = useState({
         data: [],
         loading: true
     })
 
-    setTimeout( () => {
-        setState({
-            data: [1,2,3,4],
-            loading: false
-        })
-    }, 3000);
+    useEffect(() => {
+        getGifs(categoria)
+        .then(imgs => {
+
+            setTimeout(() => {
+                console.log(imgs);
+                
+                setState({
+                    data: imgs,
+                    loading: false
+                });    
+            }, 2000);
+        });
+    }, [categoria])
 
     return state; // {data: [], loading: true};
 }
