b7df41d05ae800b6c437e7306847543e2b29caad
fadria <fadriacarrasco@gmail.com>
2022-03-02 11:26:04 +0100

    Componente AddCategory

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
 
 export const GifExpertApp = () => {
 
   //const categorias = ['Gatos', 'Perros', 'Pájaros'];
   const [categorias, setcategorias] = useState(['Gatos', 'Perros', 'Pájaros'])
 
-  const handleAdd = () => {
+  /*const handleAdd = () => {
     setcategorias(["Nutrias", ...categorias]);
     //setcategorias(cts => [...categorias, "Nutrias"]);
-  }
+  }*/
 
   return (
     <>
         <h2>GifExpertApp</h2>
-        <hr />
-
-        <button onClick={handleAdd}>Añadir</button>
+        <AddCategory/>
+        <hr />        
 
         <ol>
           {
---
+++ 04-gif-expert-app/src/components/AddCategory.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+export const AddCategory = () => {
+
+    const [inputValue, setinputValue] = useState("Escriba una categoría...");
+
+    const handleImputChange = (e) => {
+        setinputValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submit hecho");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                value={inputValue}
+                onChange={handleImputChange}
+            />
+        </form>
+    )
+}
