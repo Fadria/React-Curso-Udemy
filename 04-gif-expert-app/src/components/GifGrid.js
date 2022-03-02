19f9b056f8f90da411ccb91ec7a6e26de8dffcda
fadria <fadriacarrasco@gmail.com>
2022-03-02 11:59:54 +0100

    useEffect

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({categoria}) => {
+
+    const [count, setcount] = useState(0);
+    
+    useEffect(() => {
+        getGifs();
+    }, [])
 
     const getGifs = async() => {
         const url = "https://api.giphy.com/v1/gifs/search?q=Gatos&limit=10&api_key=zqgveaEuOJcuBtT7wokIAJZ15Jd1I61o";
@@ -16,13 +22,16 @@ export const GifGrid = ({categoria}) => {
         })
 
         console.log(gifs);
+        // setImgs
     }
 
-    getGifs();
+    //getGifs();
 
     return (
         <div>
             <h3>{categoria}</h3>
+            <h3>{count}</h3>
+            <button onClick={() => setcount(count +1)}></button>
         </div>
     )
 }
